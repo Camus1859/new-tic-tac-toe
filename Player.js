b3a15e0e17e5044007ef4d81f46d1ec0f8b838e2
Anderson Laventure <Laventurepsu1@gmail.com>
2020-09-04 10:14:40 -0400

    created Player factory

--- Player.js
+++ Player.js
@@ -1,3 +1,9 @@
-export const Player = () => {
+export const Player = (name, score, letter) => {
 
+  return {name, score, letter}
 };
+
+const player1 = Player('player1', 0, 'X')
+const player2 = Player ('player2', 0, 'O')
+
+export {player1, player2}
