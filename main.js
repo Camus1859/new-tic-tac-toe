1a540fc511bf246a8c600bcacf3ef4ce6cca9656
Anderson Laventure <Laventurepsu1@gmail.com>
2020-09-08 15:04:08 -0400

    added input buttons

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import {Player} from '/Player.js'
 import {ui} from '/UI.js'
 import {game_flow} from '/GameFlow.js'
 import {game_board} from '/GameBoard.js'
-//ui.boardClick()
 ui.displayXorO()
 ui.playAgainBtn()
 ui.resetGame()
 ui.disablePlayAgainBtn()
+ui.disableReset()
